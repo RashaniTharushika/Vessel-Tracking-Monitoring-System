16d1870a0a6bc5081627904dbc43b50b08e10588
RashaniTharushika <rashanitharushika99@gmail.com>
2022-12-16 01:07:39 +0530

    header added

---
+++ Frontend/src/components/Header.js
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./style.css";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
+
+	return (
+		<header>
+			<img src="https://seeklogo.com/images/M/mas-holdings-logo-2ACA338CD6-seeklogo.com.png" className="Logo" style={{width: '10%'}}/>
+			<nav ref={navRef}>
+				<a href="../Dashboard.jsx">Home</a>
+				<a href="/#">Operation team</a>
+				<a href="/#">Power BI dashboard</a>
+				<a href="/#">About</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button className="nav-btn" onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
