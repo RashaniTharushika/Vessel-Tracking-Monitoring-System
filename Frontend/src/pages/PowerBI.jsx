3af551dc7dbc3c02371efd15bd1a5b3547c26bcf
RashaniTharushika <rashanitharushika99@gmail.com>
2022-12-28 15:07:57 +0530

    Create PowerBI.jsx

---
+++ Frontend/src/pages/PowerBI.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const PowerBI = () => {
+
+    return (<>
+           <iframe title="Logistic Vessel Tracking and Monitoring System" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=7b1f5ed2-1e43-40f1-8818-be3678adc495&autoAuth=true&ctid=852c5799-8134-4f15-9d38-eba4296cc76f" frameborder="0" allowFullScreen="true"></iframe>
+        </>);
+};
+
+export default PowerBI;
+
