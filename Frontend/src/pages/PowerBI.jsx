51ee88ebf7e32555f23a7f670c50e7c9385cbe85
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2023-01-02 11:49:03 +0530

    update files

--- Frontend/src/pages/PowerBI.jsx
+++ Frontend/src/pages/PowerBI.jsx
@@ -4,11 +4,21 @@ import Navbar from "../components/Header";
 
 const PowerBI = () => {
 
+    const plant = localStorage.getItem('plant')
+
+    React.useEffect(() => {
+        if (!localStorage.getItem("plant")) history.push("/login");
+    }, [logout]);
+
     return (<>
-           <iframe title="Logistic Vessel Tracking and Monitoring System" width="1700" height="700" src="https://app.powerbi.com/reportEmbed?reportId=7b1f5ed2-1e43-40f1-8818-be3678adc495&autoAuth=true&ctid=852c5799-8134-4f15-9d38-eba4296cc76f" frameborder="0" allowFullScreen="true"></iframe>
-
-
-           <iframe title="Logistic Vessel Tracking and Monitoring System- MAS Active" width="1700" height="700" src="https://app.powerbi.com/reportEmbed?reportId=c673bf2c-7645-4467-9593-99f62aa2962d&autoAuth=true&ctid=852c5799-8134-4f15-9d38-eba4296cc76f" frameborder="0" allowFullScreen="true"></iframe>
+            if (plant === 'INTIMATES') {
+                <iframe title="Logistic Vessel Tracking and Monitoring System" width="1700" height="700" src="https://app.powerbi.com/reportEmbed?reportId=7b1f5ed2-1e43-40f1-8818-be3678adc495&autoAuth=true&ctid=852c5799-8134-4f15-9d38-eba4296cc76f" frameborder="0" allowFullScreen="true"></iframe>     
+            } else if (plant === 'ACTIVE') {
+                <iframe title="Logistic Vessel Tracking and Monitoring System- MAS Active" width="1700" height="700" src="https://app.powerbi.com/reportEmbed?reportId=c673bf2c-7645-4467-9593-99f62aa2962d&autoAuth=true&ctid=852c5799-8134-4f15-9d38-eba4296cc76f" frameborder="0" allowFullScreen="true"></iframe>
+            } else {
+                <h3>There is nothing to show! Please Log in</h3>
+            }
+           
                   
         </>);
 };
