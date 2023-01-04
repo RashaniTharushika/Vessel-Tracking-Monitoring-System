c65f850a38b00b2935b5f9a3960f075ed03d282b
RashaniTharushika <rashanitharushika99@gmail.com>
2023-01-05 00:56:50 +0530

    Update PowerBI.jsx

--- Frontend/src/pages/PowerBI.jsx
+++ Frontend/src/pages/PowerBI.jsx
@@ -3,12 +3,12 @@ import "../App.css";
 import Navbar from "../components/Header";
 
 const PowerBI = () => {
-    <Navbar />
     const plant = localStorage.getItem('plant')
 
     if (plant === "INTIMATES") {
         return (
             <>
+            <Navbar />
                 <iframe title="Logistic Vessel Tracking and Monitoring System" width="1700" height="700" src="https://app.powerbi.com/reportEmbed?reportId=7b1f5ed2-1e43-40f1-8818-be3678adc495&autoAuth=true&ctid=852c5799-8134-4f15-9d38-eba4296cc76f" frameborder="0" allowFullScreen="true"></iframe>     
             </>);
     }
